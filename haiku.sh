6dd1ee87429c1f814e1bd4660bc0fc8b750e9424
Kevin Wojcik <kwojcik@uber.com>
2016-11-30 10:26:34 -0800

    add punctuation and periods at the end

--- haiku.sh
+++ haiku.sh
@@ -1,6 +1,17 @@
-# Generate a haiku and place a period at the end of each line
+# Generate a haiku with some punctuation and place a period at the end of each line
 #
 # The first argument can be used as an argument to grep -E for words
-for n in 5 7 5; do
-    cat /usr/share/dict/words | grep -E "$1" | gshuf | head -n $n | tr '\n' ' ' | sed 's/ $/./g';
+#
+# protip: haiku | say
+
+for i in 5 7 5; do
+    echo -n `cat /usr/share/dict/words | grep -E "$1" | gshuf | head -n 1`;
+    for j in `seq 2 $i`; do 
+        if [ $(($RANDOM % 5)) -eq '0' ]; then
+            echo -n ','
+        fi
+        echo -n ' '
+        echo -n `cat /usr/share/dict/words | grep -E "$1" | gshuf | head -n 1`;
+    done
+    echo '.';
 done
