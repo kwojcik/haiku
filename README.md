bc0834d2f533013efe55439919c0904ebd7c182e
Kevin Wojcik <kwojcik@uber.com>
2016-11-30 10:32:42 -0800

    README

---
+++ README.md
@@ -0,0 +1,6 @@
+Generate a haiku with some punctuation and place a period at the end of each line
+
+The first argument can be used as an argument to grep -E for words
+
+protip: `./haiku.sh | say`
+
--- haiku.sh
+++ haiku.sh
@@ -1,9 +1,3 @@
-# Generate a haiku with some punctuation and place a period at the end of each line
-#
-# The first argument can be used as an argument to grep -E for words
-#
-# protip: ./haiku.sh | say
-
 for i in 5 7 5; do
     echo -n `cat /usr/share/dict/words | grep -E "$1" | gshuf | head -n 1`;
     for j in `seq 2 $i`; do 
